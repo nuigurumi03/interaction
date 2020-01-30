e67a3d74dbadd797d0cb9234abfcb56ff2748220
nuigurumi03 <nuigurumi03@gmail.com>
2020-01-30 12:07:40 +0900

    imageテーブル追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to :user
+  has_many :images
+  has_many :messages
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
