d92716d175b167cd62c47e31b2f68dfd0acaee6e
nuigurumi03 <nuigurumi03@gmail.com>
2020-01-30 11:33:01 +0900

    user登録機能完了(ウィザード形式)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  root 'posts#index'
+  root 'signup#index'
+
+  resources :signup ,only: [:index] do
+    collection do
+      get 'registration'
+      post 'registration' => 'signup#first_validation'
+      get 'sms_authentication'
+      post 'sms_authentication' => 'signup#sms_check'
+      get 'address'
+      post 'address' => 'signup#second_validation'
+      get 'done'
+    end
+  end
   
 end
